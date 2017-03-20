ca410967fd52ad67ece1b425f93c9c548bbd11a2
bugness <bugness@yandex.ru>
2017-03-20 19:45:04 +0700

    2.1.11 created base files and folders for django project

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/django.conf
+sudo ln -s /home/box/web/etc/hello.gunicorn.conf /etc/gunicorn.d/hello.conf
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
